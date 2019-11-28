970f8fc35869c1a1c10479e05368384972e5610c
HP <HP@DESKTOP-7BMB7UO>
2019-11-28 17:47:46 +0300

    my First commit
    uploading all files

---
+++ myCode/src/myCode/NextTrial.java
@@ -0,0 +1,13 @@
+package myCode;
+
+public class NextTrial {
+	public static void main(String[] args) {
+		boolean myBool;
+		
+		myBool = true;
+			System.out.println("The value of myBool;" + myBool);
+			
+		myBool = false;
+			System.out.println("The value of myBool;" + myBool);
+	}		
+}
